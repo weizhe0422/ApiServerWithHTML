5ed5e5949c9a97766fb0e38f1bec1f39d16bdb79
WeiZhe Chang <weizhe.chang@gmail.com>
2019-01-14 01:40:05 +0800

    new sample file to upload

---
+++ ApiService/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"net"
+	"net/http"
+	"time"
+)
+
+func handleDisplay(resp http.ResponseWriter, req *http.Request) {
+	var (
+		inputParams map[string][]string
+		respString  *bytes.Buffer
+	)
+
+	inputParams = make(map[string][]string, 0)
+	inputParams = req.URL.Query()
+
+	respString = bytes.NewBuffer([]byte("Request String:\n"))
+
+	for key, param := range inputParams {
+		for _, value := range param {
+			respString.Write([]byte(key + ":" + value + "\n"))
+		}
+	}
+
+	resp.Write(respString.Bytes())
+
+}
+func main() {
+	var (
+		mux            *http.ServeMux
+		httpSvr        *http.Server
+		err            error
+		listener       net.Listener
+		httpDir        http.Dir
+		webPageHandler http.Handler
+	)
+
+	httpDir = http.Dir("./webPage")
+	webPageHandler = http.FileServer(httpDir)
+
+	mux = http.NewServeMux()
+	mux.HandleFunc("/test/show", handleDisplay)
+	mux.Handle("/", http.StripPrefix("/", webPageHandler))
+
+	httpSvr = &http.Server{
+		ReadTimeout:  5 * time.Millisecond,
+		WriteTimeout: 5 * time.Millisecond,
+		Handler:      mux,
+	}
+
+	if listener, err = net.Listen("tcp", ":8080"); err != nil {
+		log.Fatalf("failed to new listener: %s", err)
+	}
+
+	go httpSvr.Serve(listener)
+
+	for {
+		time.Sleep(1 * time.Second)
+	}
+
+}
